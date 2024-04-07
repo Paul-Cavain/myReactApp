5b465df807e2b52d5eb6ceecbf5d8525e994ed4f
Paul-Cavain <paulcavain01@gmail.com>
2024-04-08 00:55:25 +0300

    added list and keys for Day 5

--- src/Home.js
+++ src/Home.js
@@ -1,49 +1,71 @@
 import { useState } from "react"
+import { BsTrash } from "react-icons/bs"
 
 const Home = () => {
 
-    // useState Hook
-    const [name, setName] = useState('Bob')
-    const [count, setCount] = useState(0)
+    // List and Keys
+    const [items, setItems] = useState([
+        {
+            id: 1, 
+            checked: true,
+            item: "item1"
+        },
+        {
+            id: 2, 
+            checked: false,
+            item: "item2"
+        },
+        {
+            id: 3, 
+            checked: false,
+            item: "item3"
+        }
+    ])
+    const handleCheck = ((id) => {
+        const listItems = items.map((item) => item.id === id ? {...item, checked: !item.checked} : item)
+        setItems(listItems)
 
-    const handleNameChange = () => {
-        const name = ['React', 'Bob', 'Justine']
-        const init = Math.floor(Math.random() *3)
-        setName(name[init])
+        //saving items to the local storage
+        localStorage.setItem('Shoppinglist', JSON.stringify(listItems));
+    })
+
+    // function for deleting an item
+    const handleDelete = (id) => {
+        const listItems = items.filter((item) => item.id !== id)
+        setItems(listItems)
+        localStorage.setItem('Shoppinglist', JSON.stringify(listItems));
     }
 
-    // event click
-    const handleClick = () =>{
-        setCount(count + 1);
-        console.log(count)
-    }
-
-    // event click with parameter
-    const handleClick2 = (name) =>{
-        console.log(count)
-    }
-    // handling event in event click
-    const handleClick3 = (e) =>{
-        console.log(e.target.innerText)
-    }
     
     return (
-        <main className="grid space-y-4 justify-center pt-[40%]">
-            {/* random name change diaplayed */}
-            <div className="mx-1">
-                <p>
-                    Hi {name}!
-                </p>
-            </div>
-            
-            <div className="grid gap-4">
-                <button onClick={handleNameChange} className="text-center bg-sky-400 py-2 w-32">Change Name</button>
-
-                <button onClick={handleClick} className="text-center bg-sky-400 py-2 w-28">ClickMyName</button>
-
-                <button onClick={handleClick2} className="text-center bg-sky-400 py-2 w-32">ClickHandleEvent</button>
-            </div>
-            
+        <main className=" pt-[5%]">
+            <h1 className="text-3xl text-center">List and Keys</h1>
+            {items.length ? (
+            <ul>
+                {items.map((item) => (
+                    <li className="items flex flex-row justify-between text-2xl m-3 px-10 bg-gray-400 py-3" key={item.id}>
+                        <div className="flex justify-start gap-2">
+                            <input
+                                className="size-10 cursor-pointer"
+                                onChange={() => handleCheck (item.id)} 
+                                type="checkbox" 
+                                checked={item.checked} 
+                            />
+                            <label onDoubleClick={() => handleCheck (item.id)} style={(item.checked) ? {textDecoration: 'line-through' } : null}>
+                                {item.item}
+                            </label>
+                        </div>
+                        <BsTrash 
+                            onClick={() => handleDelete(item.id)} 
+                            className="text-4xl text-blue-700 hover:text-red-600"
+                            role="button" tabIndex="0"
+                        />
+                    </li>
+                ))}
+            </ul>
+            ) : (
+                <p className="text-center pt-20 text-red-600 text-lg">Your List is Empty!</p>
+            )}
         </main>
     );
 }
