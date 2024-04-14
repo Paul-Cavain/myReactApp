9a1a26d9bbef70331bcfb8c5b3517a1253d06949
Paul-Cavain <paulcavain01@gmail.com>
2024-04-14 11:54:01 +0300

    solving first project challenge of add color and display its color and name on a card

---
+++ src/colorgenerator/ColorDisplay.js
@@ -0,0 +1,22 @@
+const ColorDisplay = ( {colorName, setColorName, handleColorChange} ) =>{
+    return(
+        <form className="mx-3" onSubmit={handleColorChange}>
+            <hr className="border-2 border-red-600" />
+            <h2 className="text-center mt-12 text-xl">ColorGerenerator</h2>
+            <div className="shadow-md border-2 border-black rounded-md h-40 mt-2" style={{backgroundColor: colorName}}>
+                <p 
+                className="text-center m-14 text-xl">
+                    {colorName ? colorName : 'Empty Color'}
+                </p>
+            </div>
+            <input
+                type="text"
+                placeholder="Add Color Name"
+                className="h-12 w-full px-3 mt-4 rounded-md border-2 border-black"
+                value={colorName}
+                onChange={(e) => setColorName(e.target.value)}
+            />
+        </form>
+    )
+}
+export default ColorDisplay
